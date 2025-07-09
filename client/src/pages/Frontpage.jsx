8730fe5fa0c9e97a8e0d449e3a1a4ed972a6eb2e
Ravi <ayswarym04@gmail.com>
2025-07-09 18:48:06 +0530

    Ndemon added3

--- client/src/pages/Frontpage.jsx
+++ client/src/pages/Frontpage.jsx
@@ -28,7 +28,7 @@ export default function Frontpage() {
         </div>
         <div class="hero-image-container">
           <img
-            src="./src/assets/examportalim.png"
+            src="examportalim.png"
             alt="Modern and dynamic illustration showing students interacting with digital exam portal - laptops, study material, check marks emphasizing successful exam completion in vibrant colors with clean style"
             class="hero-image"
             onerror="this.onerror=null;this.src='https://storage.googleapis.com/workspace-0f70711f-8b4e-4d94-86f1-2a93ccde5887/image/73cff934-e0bd-4662-9849-a07e3e3d6c76.png';"
